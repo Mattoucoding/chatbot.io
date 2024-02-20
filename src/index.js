c0d871a2c27bf7f89caa85391f8e611439e0c284
dylan <solon.dylan@laposte.net>
2024-02-20 16:05:51 +0100

    v1

--- src/index.js
+++ src/index.js
@@ -1,102 +1,80 @@
+import toto from './views/img/toto.jpg';
+import robot from './views/img/robot.png';
+import naruto from './views/img/naruto.jpg';
+
 import './index.scss';
 
 document.body.innerHTML = `
-<div class="container">
-<div class="chatbot">
 <header>
-  <h2>Chatbot</h2>
-  <i class="ri-close-line"></i>
+    <nav>
+        <img src="robot.png" alt=""/>
+        <h1>Chatbot.io</h1>
+    </nav>
 </header>
-<ul class="chatbox">
-  <li class="chat incoming">
-    <i class="ri-robot-2-line"></i>
-    <p>
-      Hi there <br />
-      How can I help you today?
-    </p>
-  </li>
-</ul>
-<div class="chat-input">
-  <textarea placeholder="Enter a message..." required></textarea>
-  <i class="ri-send-plane-2-line"></i>
-</div>
-</div>
-</div>
+<main>
+  <section class="left">
+    <div class="bot-container">
+        <!-- Search bar -->
+        <div class="search-bar">
+            <input type="text" placeholder="Search...">
+        </div>
+        <div class="leftItem" id="Bot-one">
+            <div class="left__robotContainer">
+                <div class="robot-icon">
+                    <img src="${toto}" alt="Robot Icon">
+                </div>
+                <h1>Goku Bot</h1>
+            </div>
+            <p>Lorem, ipsum dolor.</p>
+            <p>2min</p>
+        </div>
+        <div class="leftItem" id="Bot-two">
+            <div class="left__robotContainer">
+                <div class="robot-icon">
+                    <img src="${robot}" alt="Robot Icon">
+                </div>
+                <h1>Luffy Bot</h1>
+            </div>
+            <p>Lorem, ipsum dolor.</p>
+            <p>2min</p>
+        </div>
+        <div class="leftItem" id="Bot-three">
+            <div class="left__robotContainer">
+                <div class="robot-icon">
+                    <img src="${naruto}" alt="Robot Icon">
+                </div>
+                <h1>Naruto Bot</h1>
+            </div>
+            <p>Lorem, ipsum dolor.</p>
+            <p>2min</p>
+        </div>
+    </div>
+</section>
 
+    <section class="right">
+        <div class="containerRight" id="conv1">
+            <div class="containerRightTitle">
+                <h1>Robot français</h1>
+            </div>
+            <ul class="chatbox">
+                <li class="myTurn">
+                    <div class="message-container">
+                        <span class="message">User's message here</span>
+                        <span class="timestamp">12:35 PM, 16 Feb 2024</span>
+                    </div>
+                </li>
+                <li class="botTurn">
+                    <h2 class="robot-name" style="font-size: 0.8rem;">Robot français</h2> <!-- Robot's name with smaller font size -->
+                    <div class="message-container">
+                        <span class="message">Bot's message here</span>
+                        <span class="timestamp">12:30 PM, 16 Feb 2024</span>
+                    </div>
+                </li>
+            </ul>
+              <div class="containertextarea">
+                  <input placeholder="Write here..." type="text" class="input">
+                  <a href="" class="send"><i class="ri-send-plane-fill"></i></a>
+              </div>
+    </section>
+</main>
 `;
-
-const chatInput = document.querySelector('.chat-input textarea');
-const sendChatBtn = document.querySelector('.chat-input i');
-const chatbox = document.querySelector('.chatbox');
-
-let userMessage;
-const API_KEY = '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';
-const inputInitHeight = chatInput.scrollHeight;
-
-const createChatLi = (message, className) => {
-  // create a chat <li> element with passed message and className
-  const chatLi = document.createElement('li');
-  chatLi.classList.add('chat', className);
-  const chatContent = className === 'outgoing' ? '<p></p>' : "<i class='ri-robot-2-line'></i><p></p>";
-  chatLi.innerHTML = chatContent;
-  chatLi.querySelector('p').textContent = message;
-  return chatLi;
-};
-
-const generateResponse = (incomingChatLi) => {
-  const API_URL = 'https://api.openai.com/v1/chat/completions';
-  const messageElement = incomingChatLi.querySelector('p');
-
-  const requestOptions = {
-    method: 'POST',
-    headers: {
-      'Content-type': 'application/json',
-      Authorization: `Bearer ${API_KEY}`
-    },
-    body: JSON.stringify({
-      model: 'gpt-3.5-turbo',
-      message: [{ role: 'user', content: userMessage }]
-    })
-  };
-  // send POST request to API, get response
-  fetch(API_URL, requestOptions)
-    .then(res => res.json())
-    .then(data => {
-      console.log(data);
-    }).catch(() => {
-      messageElement.textContent = 'Oops! Something went wrong. Please try again.';
-    })
-    .finally(() => chatbox.scrollTo(0, chatbox.scrollHeight));
-};
-
-const handleChat = () => {
-  userMessage = chatInput.value.trim();
-  if (!userMessage) return;
-  chatInput.value = '';
-  chatInput.style.height = `${inputInitHeight}px`;
-
-  // append the user's message to the chatbox
-  chatbox.appendChild(createChatLi(userMessage, 'outgoing'));
-  chatbox.scrollTo(0, chatbox.scrollHeight);
-
-  setTimeout(() => {
-    // Display "Thinking..." message while waiting for the response
-    const incomingChatLi = createChatLi('Thinking...', 'incoming');
-    chatbox.appendChild(incomingChatLi);
-    chatbox.scrollTo(0, chatbox.scrollHeight);
-    generateResponse(incomingChatLi);
-  }, 600);
-};
-chatInput.addEventListener('input', () => {
-  // adjust the height of the input textarea based on its content
-  chatInput.style.height = `${inputInitHeight}px`;
-  chatInput.style.height = `${chatInput.scrollHeight}px`;
-});
-chatInput.addEventListener('keyup', (e) => {
-  if (e.key === 'Enter' && !e.shiftKey && window.innerWidth > 800) {
-    e.preventDefault();
-    handleChat();
-  }
-});
-
-sendChatBtn.addEventListener('click', handleChat);
