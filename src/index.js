37f27a4db1f1f3caf785f0b2e5d178c4daf23b43
Mattoucoding <mattcoding45@gmail.com>
2024-02-22 14:16:16 +0100

    Ajout envoi d'un message user

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import Router from './Router';
-import Search from './controllers/Search';
 import Home from './controllers/Home';
 
 import './index.scss';
@@ -7,9 +6,6 @@ import './index.scss';
 const routes = [{
   url: '/',
   controller: Home
-}, {
-  url: '/search',
-  controller: Search
 }];
 
 new Router(routes);
